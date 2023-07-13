ff41dbe081b0d4ffaca0b9315af3820d4e0ab125
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-13 23:24:15 +0530

    Add files via upload

---
+++ Weather app/script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a0e8ad559amsh8a9f120b2c4be0ap19d874jsn506081e1f34d',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
