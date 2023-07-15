582252a726afeb2a0ab033d06f4c3b06774f4b3f
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-15 16:52:27 +0530

    Add files via upload

--- Weather app/script.js
+++ Weather app/script.js
@@ -13,15 +13,15 @@ fetch(url, options)
   .then((Response) => {
     console.log(Response);
 
-    cloud_pct = response.cloud_pct;
-    feels_like = response.feels_like;
-    humidity = response.humidity;
-    max_temp = response.max_temp;
-    min_temp = response.min_temp;
-    sunrise = response.sunrise;
-    sunset = response.sunset;
-    temp = response.temp;
-    wind_degrees = response.wind_degrees;
-    wind_speed = response.wind_speed;
+    cloud_pct.innerHtml = response.cloud_pct;
+    feels_like.innerHtml = response.feels_like;
+    humidity.innerHtml = response.humidity;
+    max_temp.innerHtml = response.max_temp;
+    min_temp.innerHtml = response.min_temp;
+    sunrise.innerHtml = response.sunrise;
+    sunset.innerHtml = response.sunset;
+    temp.innerHtml = response.temp;
+    wind_degrees.innerHtml = response.wind_degrees;
+    wind_speed.innerHtml = response.wind_speed;
   })
   .then((err) => console.log(err));
